00c0bd07f4064652e7c6480d9aab72cd14a5b056
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-30 20:51:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School #Model
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    roster.collect do |grade,students|
+      roster[grade] = students.sort!
+    end
+    roster
+  end
+
+end
