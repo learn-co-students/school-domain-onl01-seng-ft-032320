8f09da57b59b7c34d90a526a4357c7561181574f
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-01 20:30:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+#defines the setter and getter
+  attr_accessor :grade
+    #defines only the getter
+  attr_reader :roster, :name
+
+ROSTER = {}
+  # Create empty roster
+  def initialize(roster)
+    @roster = roster
+    #@roster= {}
+  end
+
+  def add_student (name, grade)
+    @name = name
+    @grade = grade
+
+
+#-- create an if, if grade is already in roster, just assign the name to assocaiated key
+
+    # create a key for @grade
+    @roster[@grade] = grade
+
+    #value will be @name
+    @roster[@grade] << name
+  end
+
+
+end
