c0a5af13e40fd3d73790799df302dae1c2db799f
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-02 03:03:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+
+  attr_accessor
+  attr_reader :school, :roster
+
+
+  def initialize(school)
+    @school = school
+    @roster = roster
+    @roster= {}
+  end
+
+  def add_student(student_name, grade)
+    if !roster[grade].is_a?(Array)
+      roster[grade] =[]
+    end
+    roster[grade] << student_name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.map do |grade, name|
+      roster[grade] = name.sort
+      roster.sort
+    end
+    roster
+  end
+
+end
