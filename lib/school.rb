01af8aaee4f5c66236ed203154a37c5ba958482a
Nicholas Keller <nic.keller328@gmail.com>
2020-04-03 15:59:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  attr_reader   :name, :roster, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+    
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if @roster.empty?
+      @roster[grade] = [name]
+    elsif @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+    
+  def sort
+    key_array = @roster.keys.sort
+    i = 0
+    sort_roster = @roster
+    sorted_list = {}
+    while i < key_array.length
+      sorted_list[key_array[i]] = sort_roster[key_array[i]].sort
+      i += 1
+    end
+    sorted_list
+  end
+  
+  
+end
